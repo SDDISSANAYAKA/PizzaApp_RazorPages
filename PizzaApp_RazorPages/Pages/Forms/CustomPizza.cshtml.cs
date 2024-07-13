059abe4ce68ceedd2d0f0ed1ad51e42312a31db9
Sachintha_Dissanayaka <95671286+SDDISSANAYAKA@users.noreply.github.com>
2024-07-13 17:47:25 +0530

    Data Model

--- PizzaApp_RazorPages/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaApp_RazorPages/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaApp_RazorPages.Models;
 
 namespace PizzaApp_RazorPages.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel  Pizza { get; set; }
         public void OnGet()
         {
         }
